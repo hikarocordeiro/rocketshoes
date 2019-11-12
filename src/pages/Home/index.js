178c28aebc087a806cdb52d8af71e3cd918ce072
Hikaro Cordeiro <hikaro.cordeiro@gmail.com>
2019-11-12 15:13:23 -0300

    Home styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div />
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/26/004-5830-026/004-5830-026_detalhe1.jpg?ims=240x240"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-futsal-nike-beco-2-futsal/26/004-5830-026/004-5830-026_detalhe1.jpg?ims=240x240"
+          alt="tenis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
   );
 }
